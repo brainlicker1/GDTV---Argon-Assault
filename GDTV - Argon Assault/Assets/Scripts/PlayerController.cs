2e0f1b1996fa055c28eaf6ca7a6797507336bed4
DESKTOP-FQTBGS1\Randomality <brainlicker1@yahoo.com>
2022-03-29 18:11:17 -0400

    New Unity input system enabled, ondisable/onenable added.

--- GDTV - Argon Assault/Assets/Scripts/PlayerController.cs
+++ GDTV - Argon Assault/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] InputAction movement;
     void Start()
     {
         
@@ -14,9 +14,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       float horizontalThrow = Input.GetAxis("Horizontal");
-        Debug.Log(horizontalThrow);
-        float verticalThrow = Input.GetAxis("Vertical");
-        Debug.Log(verticalThrow);
+        UserInput();
     }
+
+    void OnEnable() {
+        movement.Enable();
+    }
+     void OnDisable() {
+        movement.Disable();
+    }
+    void UserInput(){
+
+
+            float horizontalThrow = movement.ReadValue<Vector2>().x;
+            float verticalThrow = movement.ReadValue<Vector2>().y;
+
+    }
+
+
 }
