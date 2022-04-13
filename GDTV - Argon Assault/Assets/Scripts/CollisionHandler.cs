cf0855ee1ba08ecb825047046bc80aca1ef58b4f
DESKTOP-FQTBGS1\Randomality <brainlicker1@yahoo.com>
2022-04-13 18:09:28 -0400

    Reload level on collide

--- GDTV - Argon Assault/Assets/Scripts/CollisionHandler.cs
+++ GDTV - Argon Assault/Assets/Scripts/CollisionHandler.cs
@@ -1,14 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
-{
+{       [SerializeField] float loadDelay =1f;
      void OnCollisionEnter(Collision other) {
         Debug.Log("BANG BITCH " + other.gameObject.name);
+        
+        
+
     }
 
     void OnTriggerEnter(Collider other) {
         Debug.Log("GetTriggered Bitch " + other.gameObject.name);
+        StartCrashSequence();
+       
+    }
+
+    void StartCrashSequence(){
+        GetComponent<PlayerController>().enabled = false;
+        Invoke("ReloadLevel" ,loadDelay);
+    }
+    void ReloadLevel(){
+        
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+
     }
 }
