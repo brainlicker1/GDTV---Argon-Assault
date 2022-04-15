32d7b7f75aa0ea1f0f13de5421d96eefc4c65564
DESKTOP-FQTBGS1\Randomality <brainlicker1@yahoo.com>
2022-04-15 17:25:16 -0400

    things n stuff

--- GDTV - Argon Assault/Assets/Scripts/CollisionHandler.cs
+++ GDTV - Argon Assault/Assets/Scripts/CollisionHandler.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {       [SerializeField] float loadDelay =1f;
+        [SerializeField] ParticleSystem crashSFX;
      void OnCollisionEnter(Collision other) {
         Debug.Log("BANG BITCH " + other.gameObject.name);
         
@@ -19,11 +20,15 @@ public class CollisionHandler : MonoBehaviour
 
     void StartCrashSequence(){
         GetComponent<PlayerController>().enabled = false;
+        
         Invoke("ReloadLevel" ,loadDelay);
     }
     void ReloadLevel(){
         
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<BoxCollider>().enabled = false;
+        crashSFX.Play();
         SceneManager.LoadScene(currentSceneIndex);
 
     }
