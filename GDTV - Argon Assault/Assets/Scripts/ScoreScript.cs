da9d82b31c628d6b8a8bae3c4b4a0f7942b834f0
DESKTOP-FQTBGS1\Randomality <brainlicker1@yahoo.com>
2022-04-19 18:17:11 -0400

    score txt thing.

--- GDTV - Argon Assault/Assets/Scripts/ScoreScript.cs
+++ GDTV - Argon Assault/Assets/Scripts/ScoreScript.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreScript : MonoBehaviour
 {
     int score;
+    [SerializeField] TMP_Text scoreText;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = "Score: ";
+    }
    public void IncreaseScore(int scoreAmmount){
 
        score += scoreAmmount; 
-
-
+       scoreText.text = score.ToString();
    }
 }
