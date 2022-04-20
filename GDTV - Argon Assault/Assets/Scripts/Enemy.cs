512f98d3df52fab41f4f232f06f3fcb77ce4483e
DESKTOP-FQTBGS1\Randomality <brainlicker1@yahoo.com>
2022-04-20 16:47:03 -0400

    Hitpoints

--- GDTV - Argon Assault/Assets/Scripts/Enemy.cs
+++ GDTV - Argon Assault/Assets/Scripts/Enemy.cs
@@ -8,21 +8,29 @@ public class Enemy : MonoBehaviour
     [SerializeField] GameObject deathVfx;
     [SerializeField] Transform parent;
     [SerializeField] int enemyScoreValue = 500;
+    [SerializeField] int enemyHitpoints = 2;
     // Start is called before the first frame update
 
     void Start() {
      scoreScript = FindObjectOfType<ScoreScript>();
+
     }
      void OnParticleCollision(GameObject other) {
+
+         
          ScoreIncrease();
-        GameObject vfx = Instantiate(deathVfx,transform.position, Quaternion.identity);
+         if(enemyHitpoints <= 0) {
+
+              GameObject vfx = Instantiate(deathVfx,transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         Destroy(gameObject);
+         }
+       
     }
 
     void ScoreIncrease(){
-
-scoreScript.IncreaseScore(enemyScoreValue);
+        enemyHitpoints--;
+        scoreScript.IncreaseScore(enemyScoreValue);
 
     }
 
